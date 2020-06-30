44305951331ab6c089e40fdecb2217f69a74a0f7
Simon <Simonbolsen1234@gmail.com>
2020-06-30 22:45:14 +0200

    Test testing

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -1,3 +1,17 @@
-class Test {
+import org.scalatest.funsuite.AnyFunSuite
 
+
+class Test extends AnyFunSuite {
+
+    test("1 times 0 should be 0") {
+        assert(Syntax.func(0, 1.0f) == 0)
+    }
+
+    test("10 times 20 should be 200") {
+        assert(Syntax.func(10, 20.0f) == 200)
+    }
+
+    test("1 times 20 should be 200") {
+        assert(Syntax.func(1, 20.0f) == 0)
+    }
 }
