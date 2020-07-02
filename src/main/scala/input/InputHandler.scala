42b198433129ee9e91a58350248d9012538c352c
Malteafg <malteut@gmail.com>
2020-07-02 17:46:12 +0200

    MousePos vector

--- src/main/scala/input/InputHandler.scala
+++ src/main/scala/input/InputHandler.scala
@@ -1,24 +1,23 @@
 package input
 
+import math.Vector2f
 import utils.Options
 
 object InputHandler {
 
-    var x: Int = 0
-    var y: Int = 0
+    var mousePos: Vector2f = new Vector2f()
 
     def keyPressed(window: Long, key: Int, scancode: Int, action: Int, mods: Int): Unit = {
         Options.log(s"Key \'${key.toChar}\' was pressed", Options.Keys)
     }
 
     def mousePressed(window: Long, button: Int, action: Int, mods: Int): Unit = {
-        Options.log(s"Button ${button} was pressed on (${x}, ${y})", Options.Mouse)
+        Options.log(s"Button ${button} was pressed on (${mousePos.x}, ${mousePos.y})", Options.Mouse)
     }
 
     def mouseMoved(window: Long, xpos: Double, ypos: Double): Unit = {
-        x = xpos.toInt
-        y = ypos.toInt
-        Options.log(s"Mouse was moved to (${x}, ${y})", Options.Mouse)
+        mousePos.set(xpos.toInt, ypos.toInt)
+        Options.log(s"Mouse was moved to (${mousePos.x}, ${mousePos.y})", Options.Mouse)
     }
 
 }
--- src/main/scala/utils/Options.scala
+++ src/main/scala/utils/Options.scala
@@ -5,25 +5,23 @@ object Options {
     abstract class Log {
         var logging: Boolean = false
         var parent: Log = null
+        var name: String = "Log"
         def log(): Boolean = if(logging) true else if(parent == null) logging else {
-            logging = parent.log();
+            logging = parent.log()
             logging
         }
     }
+
     case object Log extends Log
-    case object Input extends Log { parent = Log }
-    case object Mouse extends Log { parent = Input }
-    case object Keys extends Log { parent = Input }
+    case object Input extends Log { parent = Log; name = "Input" }
+    case object Mouse extends Log { parent = Input; name = "Mouse" }
+    case object Keys extends Log { parent = Input; name = "Keys" }
+
+    var commands = List(Log, Input, Mouse, Keys)
 
     def setLogging(args: Array[String]): Unit = {
-        for (x <- args) x match {
-            case "Input" => Input.logging = true
-            case "Mouse" => Mouse.logging = true
-            case "Keys" => Keys.logging = true
-        }
-        Input.log()
-        Mouse.log()
-        Keys.log()
+        for (a <- args; c <- commands) if (a == c.name) c.logging = true
+        for (c <- commands) c.log()
     }
 
     def log(message: String, t: Log): Unit = if(t.logging) println(message)
