82b172cff4e4be143e78073ba78830b3ff606fb1
Simon <Simonbolsen1234@gmail.com>
2020-07-02 15:18:56 +0200

    Optional print

--- src/main/scala/input/InputHandler.scala
+++ src/main/scala/input/InputHandler.scala
@@ -4,21 +4,22 @@ import org.lwjgl.glfw.GLFW.glfwGetCursorPos
 
 object InputHandler {
 
+    val printEvents: Boolean = false
     var x: Int = 0
     var y: Int = 0
 
     def keyPressed(window: Long, key: Int, scancode: Int, action: Int, mods: Int): Unit = {
-        print(s"Key \'${key.toChar}\' was pressed \n")
+        if(printEvents) print(s"Key \'${key.toChar}\' was pressed \n")
     }
 
     def mousePressed(window: Long, button: Int, action: Int, mods: Int): Unit = {
-        print(s"Button ${button} was pressed on (${x}, ${y}) \n")
+        if(printEvents) print(s"Button ${button} was pressed on (${x}, ${y}) \n")
     }
 
     def mouseMoved(window: Long, xpos: Double, ypos: Double): Unit = {
         x = xpos.toInt
         y = ypos.toInt
-        print(s"Mouse was moved to (${x}, ${y}) \n")
+        if(printEvents) print(s"Mouse was moved to (${x}, ${y}) \n")
     }
 
 }
