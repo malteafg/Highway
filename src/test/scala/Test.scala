a1c49cdb12081326ba558875dab2df471abf011d
Simon <Simonbolsen1234@gmail.com>
2020-07-30 13:05:34 +0200

    doubleSnap fix

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -1,4 +1,5 @@
 import org.scalatest.funsuite.AnyFunSuite
+import utils.Bezier
 import utils.math.Vec3
 
 
@@ -16,6 +17,11 @@ class Test extends AnyFunSuite {
 
     test("Rays should be parallel") {
         val v = Vec3().intersection(Vec3(0, 0, 1), Vec3(2), Vec3(0, 0, -1))
+        assert(v == null)
+    }
+
+    test("Double snap should not be accepted") {
+        val v = Bezier.doubleSnapCurve(Vec3(), Vec3(0.01f, 0, 1), Vec3(2), Vec3(-1, 0, 0.01f), 2)
         assert(v == null)
     }
 
